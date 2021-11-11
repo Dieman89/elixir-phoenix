d576884e68a207d3fc5c0987385252b61b7c19ec
Alessandro <alessandro.buonerba@kaluza.com>
2021-11-11 00:16:56 +0000

    some methods in the Cards module

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,24 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
 
-  @doc """
-  Hello world.
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    for suit <- suits, value <- values do
+        "#{value} of " <> "#{suit}"
+    end
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contain?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
 end
