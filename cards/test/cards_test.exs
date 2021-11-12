d3cf39e43341e8c3357453973730a9a36df6b591
Alessandro <alessandro.buonerba@kaluza.com>
2021-11-12 22:16:31 +0000

    More code after tasty bbq chicken wings

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -3,19 +3,40 @@ defmodule CardsTest do
   doctest Cards
 
   test "create_deck makes 20 cards" do
-    deck = Cards.create_deck
+    deck = Cards.create_deck()
     deck_length = length(deck)
     assert deck_length == 20
   end
 
   test "shuffle changes the position of the cards in a deck" do
-    deck = Cards.create_deck
+    deck = Cards.create_deck()
     shuffled_deck = Cards.shuffle(deck)
     refute deck == shuffled_deck
   end
 
   test "contain? method returns true if a member is present in the list" do
-    deck = Cards.create_deck
+    deck = Cards.create_deck()
     assert Cards.contain?(deck, "Ace of Spades") == true
+  end
+
+  test "deal method returns a tuple that contains two lists, and check that the first list has the right length" do
+    deck = Cards.create_deck()
+
+    two_lists =
+      Cards.deal(deck, 2)
+      |> Tuple.to_list()
+
+    assert length(two_lists) == 2
+
+    size_deal =
+      Cards.deal(deck, 2)
+      |> elem(0)
+      |> length
+
+    assert size_deal == 2
+  end
+
+  test "create_hand method deals the right amount of cards" do
+    length(Cards.create_hand(5)) == 5
   end
 end
