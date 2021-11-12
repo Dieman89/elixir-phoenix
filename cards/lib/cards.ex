a1dc2c611811e5f06b97588ccd168de3c5c8aacc
Alessandro <alessandro.buonerba@kaluza.com>
2021-11-12 18:09:12 +0000

    Fully documented module and methods

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,6 +2,20 @@ defmodule Cards do
   @moduledoc """
   Provides methods for creating and handling a deck of cards.
   """
+
+  @doc """
+  Returns a list of strings representing a deck of playing cards.
+
+  ## Examples
+
+      iex> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -11,17 +25,63 @@ defmodule Cards do
     end
   end
 
+  @doc """
+  Shuffles a deck of cards.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+      iex> Cards.shuffle(deck)
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  @doc """
+  Checks if a card is present in a deck.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+      iex> Cards.contain?(deck, "Two of Spades")
+      true
+  """
 
   def contain?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Returns a tuple containing the hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should the hand include.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  @doc """
+  Returns a list of cards with a specified size.
+
+  ## Examples
+
+        iex> Cards.create_hand(1)
+  """
 
   def create_hand(hand_size) do
     {hand, _remaining} =
@@ -32,10 +92,28 @@ defmodule Cards do
     hand
   end
 
+  @doc """
+  Save a deck into a specified file.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.save(deck, "deck")
+  """
+
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  @doc """
+  Load a deck from a previously saved file.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.save(deck, "deck")
+      iex> Cards.load("deck")
+  """
 
   def load(filename) do
     case File.read(filename) do
